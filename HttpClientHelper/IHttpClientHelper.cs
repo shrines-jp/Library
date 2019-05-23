4d52d3933bd8c1877ad713e93575456297ba4e49
shrines <shrines@outlook.jp>
2019-05-24 00:30:50 +0900

    nuget repair

--- HttpClientHelper/IHttpClientHelper.cs
+++ HttpClientHelper/IHttpClientHelper.cs
@@ -1,22 +1,24 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using static HttpClientHelper.HttpClientHelper;
 
 namespace HttpClientHelper
 {
 	interface IHttpClientHelper
 	{
 		dynamic InvokeDataServiceQuery(string queryString);
-		dynamic InvokeDataServiceQuery(string queryString, ClientHelpers.RequestType type);
+		dynamic InvokeDataServiceQuery(string queryString, RequestType type);
 
 		string InvokeWebApiJson(Uri webApiUri, JObject jsonData);
 		string InvokeWebApiJson(Uri webApiUri, System.Text.Encoding encoding, JObject jsonData);
 
 		string InvokeWebApiJsonWebRequest(Uri webApiUri, JObject jsonData);
 
-		string InvokeWebApiHttpVerb(WEBZEN.GCPS.ClientHelpers.ClientHelpers.HttpVerb httpVerb, Uri webApiUri);
-		string InvokeWebApiHttpVerb(WEBZEN.GCPS.ClientHelpers.ClientHelpers.HttpVerb httpVerb, Uri webApiUri, System.Text.Encoding encoding);
-		string InvokeWebApiHttpVerb(WEBZEN.GCPS.ClientHelpers.ClientHelpers.HttpVerb httpVerb, Uri webApiUri, System.Text.Encoding encoding, JObject jsonData);
+		string InvokeWebApiHttpVerb(HttpVerb httpVerb, Uri webApiUri);
+        string InvokeWebApiHttpVerb(HttpVerb httpVerb, Uri webApiUri, System.Text.Encoding encoding);
+        string InvokeWebApiHttpVerb(HttpVerb httpVerb, Uri webApiUri, Encoding encoding, JObject jsonData);
 	}
 }
